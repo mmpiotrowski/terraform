c8fd8ce86cf65e67b0a7576083215dbe1b40fc52
Michał Piotrowski <michal@mpprojekt.org>
2024-05-29 19:26:25 +0200

    Changes in build scritps

--- build-all.sh
+++ build-all.sh
@@ -16,7 +16,7 @@ UPDATED=0
 
 if ! git ls-remote --tags https://github.com/mmpiotrowski/terraform.git | grep -q "${CURRENT_TERRAFORM_VERSION}-${BASE_IMAGE_AWS_CLI_V1}"; then
    ./build-and-push.sh $CURRENT_TERRAFORM_VERSION "$BASE_IMAGE_AWS_CLI_V1"
-   git tag "${CURRENT_TERRAFORM_VERSION}-${BASE_IMAGE_AWS_CLI_V1}"
+   git tag "${CURRENT_TERRAFORM_VERSION}"
    UPDATED=1
 fi
 
